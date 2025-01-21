d0136ac9670eb70baffd60f82c2eed9825f370f0
MrB14 <132985702+MrB14@users.noreply.github.com>
2025-01-21 15:57:25 +0700

    Delete indico-store directory

--- indico-store/output.tf
+++
@@ -1,7 +0,0 @@
-output "eks_cluster_name" {
-  value = module.eks.cluster_name
-}
-
-output "s3_bucket_name" {
-  value = aws_s3_bucket.static_files.bucket
-}
--- indico-store/resources/s3.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3_bucket" "static_files" {
-  bucket = "indico-store-static-files-${random_string.suffix.result}"
-  acl    = "public-read"
-}
